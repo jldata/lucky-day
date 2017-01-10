ac5fcb6aac16eb2ef35f0e3c5a87c3a72fbc9a1d
Cedric-song <kengmun@126.com>
2017-01-10 13:11:36 +0800

    a lottery project base on javascript

---
+++ public/json/local.js
@@ -0,0 +1,72 @@
+var localdata = [
+{
+  "userName": "(1)",
+  "imageUrl": "./public/image/(1).png",
+  "leader": true
+},
+{
+  "userName": "(2)",
+  "imageUrl": "./public/image/(2).png",
+  "leader": true
+},
+{
+  "userName": "(3)",
+  "imageUrl": "./public/image/(3).png",
+  "leader": true
+},
+{
+  "userName": "(4)",
+  "imageUrl": "./public/image/(4).png",
+  "leader": true
+},
+{
+  "userName": "(5)",
+  "imageUrl": "./public/image/(5).png",
+  "leader": false
+},
+{
+  "userName": "(6)",
+  "imageUrl": "./public/image/(6).png",
+  "leader": false
+},
+{
+  "userName": "(7)",
+  "imageUrl": "./public/image/(7).png",
+  "leader": false
+},
+{
+  "userName": "(8)",
+  "imageUrl": "./public/image/(8).png",
+  "leader": false
+},
+{
+  "userName": "(9)",
+  "imageUrl": "./public/image/(9).png",
+  "leader": false
+},
+{
+  "userName": "(10)",
+  "imageUrl": "./public/image/(10).png",
+  "leader": false
+},
+{
+  "userName": "(11)",
+  "imageUrl": "./public/image/(11).png",
+  "leader": false
+},
+{
+  "userName": "(12)",
+  "imageUrl": "./public/image/(12).png",
+  "leader": false
+},
+{
+  "userName": "(13)",
+  "imageUrl": "./public/image/(13).png",
+  "leader": false
+},
+{
+  "userName": "(14)",
+  "imageUrl": "./public/image/(14).png",
+  "leader": false
+},
+]
